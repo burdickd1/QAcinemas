d26aa4138c70903aa8540809c330047b5699d24c
kat <kat@javaspr3>
2017-06-22 15:50:13 +0000

    jpa demo

---
+++ SpringDemos/SpringBootWebMVCjpa/src/main/java/com/paul/SpringBootWebMvCjpaApplication.java
@@ -0,0 +1,12 @@
+package com.paul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebMvCjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebMvCjpaApplication.class, args);
+	}
+}
